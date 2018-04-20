4bc060734a66a4f83ed73d7d2ad88e85cb4eb550
Aaron Scherer <aequasi@gmail.com>
2018-04-20 10:32:26 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started
+
+1. `git clone -b github-top-5-starred --depth 1 https://github.com/aequasi/homework`
+2. `npm install`
+3. `npm start`
---
+++ index.ts
@@ -0,0 +1,53 @@
+import chalk from "chalk";
+import * as GitHub from "github-api";
+import * as prompt from "prompt";
+import * as Table from "cli-table";
+import * as ora from "ora";
+
+const search = (new GitHub()).search();
+
+
+(
+    async () => {
+        const language = await askForLanguage();
+
+        const spinner = ora(chalk.green(`Searching for high-starred ${language} repos`)).start();
+        const repos = await search.forRepositories({
+            q:    `language:${language}`,
+            sort: "stars",
+        });
+        const firstFive = repos.data.splice(0, 5);
+        spinner.succed();
+
+        const table = new Table({head: ["Name", "Description", "Stars"]});
+        firstFive.forEach(x => {
+            table.push([x.full_name, x.description, x.stargazers_count])
+        });
+
+        console.log("\n\n" + table.toString());
+    }
+)();
+
+async function askForLanguage() {
+    return new Promise((resolve, reject) => {
+        prompt.message   = chalk.yellow("Question");
+        prompt.delimiter = chalk.yellow(":");
+
+        prompt.start();
+
+        prompt.get({
+            properties: {
+                language: {
+                    description: chalk.blue(" What language would you like to see?"),
+                    required:    true,
+                }
+            }
+        }, (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve(result.language);
+        })
+    })
+}
